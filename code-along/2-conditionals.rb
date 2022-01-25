e49a4e4bd733f474ee6cfbe9ce6ebff6e1ca8ca9
gupod <gustavo.pires@kellogg.northwestern.edu>
2022-01-25 15:43:25 +0000

    c5

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,57 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+this_is_true=true
+puts this_is_true
 
-# Boolean Expressions
+this_is_false=false
+puts this_is_false
+
+# Boolean Expressions - that's the condition on logic
+puts 3==2 #testing equality 
+puts 3!=2 #non-equality
+puts 3>2
+puts 3<2
 
 # If Conditional Logic
+if 3==2
+    puts "this should never run"
+end
+
+if 3>2 
+    puts "yeah, math"
+end
 
 # If/Else Conditional Logic
+if 3==2
+    puts "this should never run"
+else
+    puts "this should be displayed"
+end
+
+user_entered_pwd = "tacos"
+real_pwd = "secret"
+
+if user_entered_pwd==real_pwd
+    puts "you're in"
+else 
+    puts "wrong pwd"
+end
 
 # Elsif Conditional Logic
+your_team_score = 1
+other_team_score = 2
+if your_team_score > other_team_score
+    puts "you won!"
+elsif your_team_score == other_team_score
+    puts "it's a tie"
+else 
+    puts "you lost"
+end 
 
 # Combining Expressions
+temp = 68
+precip = 0
+if temp >= 65 && temp <=90 && precip ==0
+    puts "it's perfect out"
+end 
