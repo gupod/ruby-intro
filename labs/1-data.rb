202d0fc222d156d8833a61b6cf80235db4ac235f
gupod <gustavo.pires@kellogg.northwestern.edu>
2022-01-25 15:08:39 +0000

    c4

--- labs/1-data.rb
+++ labs/1-data.rb
@@ -10,6 +10,12 @@
 # then write them out to the screen. Show the total
 # as well.
 
+dice_1 = rand(1..6)
+dice_2 = rand(1..6)
+
+puts "Dice 1: #{dice_1}"
+puts "Dice 2: #{dice_2}"
+
 # HINTS
 # Get a random number between 1 and 6 by doing:
 # rand(1..6)
