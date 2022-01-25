21178bfe4e3553afffdf2939d80a17f2dd863032
gupod <gustavo.pires@kellogg.northwestern.edu>
2022-01-25 14:44:33 +0000

    c1

--- code-along/1-data.rb
+++ code-along/1-data.rb
@@ -5,8 +5,22 @@
 # In Ruby, there are different *types* of data:
 
 # Numbers
+puts 5
+puts 2
 
 # Perform simple math with numbers
+puts 5+2
+puts 5-2
+puts 5*2
+puts 5/2
+
+# integer vs float
+
+puts 5.0/2.0
+
+# order of operations
+
+puts (2+5)*5
 
 # Strings
 
